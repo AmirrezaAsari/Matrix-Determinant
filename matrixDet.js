5c5dc74c811ecb5db232854011be2457680cda35
unknown <amirreza.asari2@gmail.com>
2023-12-30 00:50:16 +0330

    Rezaie Far's Method

--- matrixDet.js
+++ matrixDet.js
@@ -73,16 +73,44 @@ function rowAndColumn(matrix) {
     for (let i = 0; i < n; i++) {
         const subMatrix = matrix.slice(1).map(row => row.filter((_, j) => j !== i));
         const sign = i % 2 === 0 ? 1 : -1;
-        det += sign * matrix[0][i] * determinant(subMatrix);
+        det += sign * matrix[0][i] * rowAndColumn(subMatrix);
     }
-
     return det;
 }
 
-const filePath = 'undefined.txt';
+function rezaieFar(matrix){
+    const n = matrix.length;
+
+    if (n !== matrix[0].length) {
+        return undefined;
+    }
+
+    if (n === 1) {
+        return matrix[0][0];
+    }
+
+    else if (n === 2) {
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+    }
+
+
+    let A = matrix.slice(1).map(row => row.filter((_, j) => j !== 0));
+    let B = matrix.slice(1).map(row => row.filter((_, j) => j !== n));
+    let C = matrix.slice(n).map(row => row.filter((_, j) => j !== 0));
+    let D = matrix.slice(n).map(row => row.filter((_, j) => j !== n));
+    let Etemp = matrix.slice(1).map(row => row.filter((_, j) => j !== 0));
+    let E = Etemp.slice(n-1).map(row => row.filter((_, j) => j !== n-1));
+    
+    let det = ((rezaieFar(A) * rezaieFar(D)) - (rezaieFar(B) * rezaieFar(C))) / rezaieFar(E);
+    
+}
+
+const filePath = '0.txt';
 
 const matrix = parseMatrixFromFile(filePath);
 const detGaussian = GaussianElimination(matrix);
 const detRowCol = rowAndColumn(matrix);
+const detRezaieFar = rezaieFar(matrix);
 console.log(`Gaussian Elimination : ${detGaussian}`);
 console.log(`row and column : ${detRowCol}`);
+console.log(`Rezaie Far : ${detRezaieFar}`);
